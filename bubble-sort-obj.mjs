dfb35bd1e606382dbed248af430b72e9c5d9578d
XandyGomes <x3.computadores@hotmail.com>
2022-03-21 20:02:01 -0300

    Aula

--- bubble-sort-obj.mjs
+++ bubble-sort-obj.mjs
@@ -12,5 +12,12 @@ function bubbleSort(vetor, fnComp) {
 }
 import {objMotoristas} from './data/motoristas-obj-desord.mjs'
 
-bubbleSort(objMotoristas, (elem1, elem2) => elem1.nome_motorista > elem2.nome_motorista)
+// bubbleSort(objMotoristas, (elem1, elem2) => elem1.nome_motorista > elem2.nome_motorista)
+// console.log(objMotoristas)
+bubbleSort(objMotoristas, (elem1, elem2) => {
+    if(elem1.razao_social === elem2.razao_social){
+        return elem1.nome_motorista > elem2.nome_motorista
+    }
+    else return elem1.razao_social > elem2.razao_social
+})
 console.log(objMotoristas)

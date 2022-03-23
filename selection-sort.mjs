ed5067863954ea5b37c5b011442fd591e3009cd2
XandyGomes <x3.computadores@hotmail.com>
2022-03-23 11:01:22 -0300

    Aula

--- selection-sort.mjs
+++ selection-sort.mjs
@@ -1,25 +1,17 @@
 let nums = [77, 44, 22, 33, 99, 55, 88, 0, 66, 11]
-let pass, comp, troca
+
 function selectionSort(vetor){
-    pass = 0
-    comp = 0
-    troca = 0
-    for(let posSel = 0; posSel < vetor.length -1; posSel++){
-        pass++
+    for(let posSel=0; posSel<vetor.length -1;posSel++){
         let posMenor = posSel + 1
         for(let i = posMenor + 1; i < vetor.length; i++){
-            if(vetor[posMenor]>vetor[i]){
+            if(vetor[posMenor] > vetor[i]){
                 posMenor = i
             }
-            comp++
         }
-
-        if(vetor[posSel]>vetor[posMenor]){
-            [vetor[posSel], vetor[posMenor]] = [vetor[posMenor], vetor[posSel]]
-            troca++
+        if(vetor[posSel] > vetor[posMenor]){
+            [ vetor[posSel], vetor[posMenor] ] = [ vetor[posMenor], vetor[posSel] ]
         }
     }
 }
 selectionSort(nums)
 console.log(nums)
-console.log({pass, comp, troca})
